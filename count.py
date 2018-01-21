f2a2808db993a660e3cfcaf67aa78b7175d208f7
Vyom Shrivastava <vyom.shrivastava1@gmail.com>
2018-01-20 19:17:42 -0500

    Program to get word frequency

---
+++ count.py
@@ -0,0 +1,18 @@
+import findspark
+findspark.init()
+import pyspark
+from operator import add
+import json
+from string import punctuation
+import math
+
+sc = pyspark.SparkContext('local',appName="WordCount")
+rdd1 = sc.wholeTextFiles("/home/vyom/UGA/DSP/P0trial/data/")
+rdd2= rdd1.values()
+splt = rdd2.flatMap(lambda word: word.split())
+splt = splt.map(lambda word: word.lower())
+word = splt.map(lambda word : (word,1))
+reduced = word.reduceByKey(add)
+filtered = reduced.filter(lambda x: x[1] > 2)
+filtered = filtered.sortBy(lambda x: x[1],False)
+print(filtered.take(5))
