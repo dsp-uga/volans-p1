e5a77bad70b03e7a65b3177763de8a7903889ff3
Vyom Shrivastava <vyom.shrivastava1@gmail.com>
2018-01-25 23:59:39 -0500

    Naive Bayes (BagOfWords) and tf-idf implementation
    This code has implementation of tf-idf values and Naive Bayes (Which is not currently utilizing tf-idf)

--- P1.py
+++ P1.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[1]:
+
 import pyspark
 from operator import add, itemgetter
 import json
@@ -10,6 +15,7 @@ from string import punctuation
 # # Calculating count of words in all docs combined
 
 # In[2]:
+
 
 sc = pyspark.SparkContext('local',appName="DocClassification")
 
@@ -61,6 +67,13 @@ CountinAllDocs = word.reduceByKey(add)
 CountinAllDocs = CountinAllDocs.sortBy(lambda x: x[1],False)
 
 
+# In[9]:
+
+
+numWords = sc.broadcast(CountinAllDocs.count())
+numDocs = sc.broadcast(documents.count())
+
+
 # # Calculating term frequency in each doc
 
 # In[10]:
@@ -100,7 +113,7 @@ def countWord(x):
     return dictionary
 
 
-# In[46]:
+# In[13]:
 
 
 bagOfWords = documents.map(lambda word: word.lower().split())
@@ -144,19 +157,14 @@ def initializeMap(x):
 occurences = sc.parallelize(uniqueList.map(lambda x: initializeMap(x)).reduce(add))
 occurences = occurences.reduceByKey(add)
 
+
 # In[18]:
 
 
-NumDocs = documents.count()
+idf = occurences.map(lambda x: (x[0],math.log(numDocs.value/x[1])))
 
 
 # In[19]:
-
-
-idf = occurences.map(lambda x: (x[0],math.log(NumDocs/x[1])))
-
-
-# In[20]:
 
 
 idf2 = sc.broadcast(idf.collectAsMap())
@@ -164,7 +172,7 @@ idf2 = sc.broadcast(idf.collectAsMap())
 
 # # Calculating Tf-idf
 
-# In[21]:
+# In[20]:
 
 
 def getTfidf(x):
@@ -175,15 +183,238 @@ def getTfidf(x):
     return tempDict
 
 
-# In[22]:
+# In[21]:
 
 
 tfidf = tf.map(lambda x: getTfidf(x) )
 
 
-# In[32]:
+# In[22]:
 
 
 tfidfSorted = tfidf.map(lambda x: sorted(x.items(), key=itemgetter(1), reverse = True))
-print(tfidfSorted.collect()[0][0:10])
+
+
+# # Implementing Naive Bayes
+
+# ## Calculating word probability
+
+# In[23]:
+
+
+spltLabels = labels.map(lambda word: word.split(","))
+
+
+# In[24]:
+
+
+def removeUnnecessary(label):
+    tempList=[]
+    for word in label:
+        if 'CAT' in word:
+            tempList.append(word)
+    return tempList
+
+
+# In[25]:
+
+
+requiredLabels = spltLabels.map(lambda label: removeUnnecessary(label))
+
+
+# In[26]:
+
+
+numberOfDocs = sc.broadcast(len(requiredLabels.reduce(add)))
+
+
+# In[27]:
+
+
+labelsToUse = sc.broadcast(requiredLabels.collect())
+
+
+# In[28]:
+
+
+def dictToList(x):
+    tempList=[]
+    for k, v in x.items():
+        tempList.append((k,v))
+    return tempList
+
+
+# In[29]:
+
+
+def getProbDoc(x,v):
+    tempDict={}
+    for label in labelsToUse.value[v]:
+        tempDict[label] = x
+    return tempDict
+
+
+# In[30]:
+
+
+ProbDocIndex = bagOfWords.zipWithIndex()
+
+
+# In[31]:
+
+
+ProbDoc = ProbDocIndex.map(lambda x: getProbDoc(x[0],x[1]) )
+
+
+# In[32]:
+
+
+ProbDocList= ProbDoc.map(lambda x : dictToList(x))
+
+
+# In[33]:
+
+
+ProbDocList = sc.parallelize(ProbDocList.reduce(add)).reduceByKey(add)
+
+
+# In[34]:
+
+
+labelWC = ProbDocList.map(lambda x: (x[0],len(x[1])))
+labelWordCount =sc.broadcast(labelWC.collectAsMap())
+
+
+# In[35]:
+
+
+def countWord2(x):
+    dictionary={}
+    for word in x[1]:
+        if word not in dictionary:
+            dictionary[word] = 1
+        else:
+            dictionary[word]+=1
+    return (x[0],dictionary)
+
+
+# In[36]:
+
+
+ProbDocListCount = ProbDocList.map(lambda x: countWord2(x))
+
+
+# In[37]:
+
+
+def getWordProbability(x):
+    tempDict={}
+    for k,v in x[1].items():
+        tempDict[k]= v/labelWordCount.value[x[0]]
+    return (x[0],tempDict)
+
+
+# In[38]:
+
+
+wordProbability = ProbDocListCount.map(lambda x: getWordProbability(x))
+
+
+# In[39]:
+
+
+def getLogProb(x):
+    tempDict= {}
+    for k,v in x[1].items():
+        tempDict[k] = math.log(v)
+    return (x[0],tempDict)
+
+
+# In[40]:
+
+
+logProbability = wordProbability.map(lambda x: getLogProb(x))
+
+
+# ## Calculating Class Probability
+
+# In[41]:
+
+
+classList = sc.parallelize(requiredLabels.reduce(add))
+classCount = classList.map(lambda x: (x,1)).reduceByKey(add)
+
+
+# In[42]:
+
+
+classProbability = classCount.map(lambda x: (x[0],x[1]/numberOfDocs.value))
+classProb = sc.broadcast(classProbability.collectAsMap())
+
+# #  Prediction
+
+# In[45]:
+
+
+testDocuments = sc.textFile("/home/vyom/UGA/DSP/Project1/X_test_vsmall.txt")
+testLabels = sc.textFile("/home/vyom/UGA/DSP/Project1/y_test_vsmall.txt")
+
+
+# In[46]:
+
+
+bagOfWordsTest = testDocuments.map(lambda word: word.lower().split())
+bagOfWordsTest = bagOfWordsTest.map(lambda x: stripWord(x))
+bagOfWordsTest = bagOfWordsTest.map(lambda x: stripStopWord(x))
+
+
+# In[47]:
+
+
+testData = sc.broadcast(bagOfWordsTest.collect())
+
+
+# In[48]:
+
+
+def TestLogProbSum(x):
+    tempDict={}
+    for i in range(len(testData.value)):
+        logSum=0;
+        for word in testData.value[i]:
+            if word in x[1]:
+                logSum=logSum+x[1][word]
+            else:
+                logSum = logSum+ 1/labelWordCount.value[x[0]]
+        tempDict[i]= logSum
+    return (x[0],tempDict)
+
+
+# In[49]:
+
+
+logProbSum = logProbability.map(lambda x: TestLogProbSum(x))
+
+
+# In[50]:
+
+
+def getPrediction(x):
+    tempDict={}
+    for k,v in x[1].items():
+        tempDict[k]= v*classProb.value[x[0]]
+    return(x[0],tempDict)
+
+
+# In[51]:
+
+
+prediction = logProbSum.map(lambda x: getPrediction(x))
+
+
+# In[52]:
+
+
+for i in prediction.collect():
+    print(i)
 
